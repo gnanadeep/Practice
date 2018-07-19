ffc2b08057968a0ebd0aca3e230ddbfccbe2b041
gnana <chandugnanadeep@gmail.com>
2018-07-19 15:06:59 -0400

    Create day3.js

---
+++ day3.js
@@ -0,0 +1,19 @@
+Day 3;
+var a = 2;
+typeof a; // number
+
+a = "hi";
+typeof a; // string
+
+a = true;
+typeof a; // boolean
+  
+a = [];
+typesof a;// object
+
+a = {};
+typesof a;//object
+
+a = function(){
+}
+typesof a;//function
