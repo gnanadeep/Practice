d594a57b2625e0be5733719adfa691658d5de4b9
chandu <chandugnanadeep@gmail.com>
2018-07-19 00:03:24 -0400

    done

--- day11.js
+++ day11.js
@@ -10,18 +10,49 @@ console.log(name,age);
 return display();
 }
 
-
-function operations() {
-debugger;
-    let counter = 0;
-	 this.getValue = function() {
-        return this.counter;
-    };
-
-    this.setValue = function(counter) {
-        this.counter = counter;
-    };
-
-	
-      return counter; 
-}
+// Write a closure which has a private variable initialised to 0.
+//  Return an object which contains four methods each takes a paramter and perform a 
+// mathematical operation with avilable private varible and return the result.
+//  Also provide a method to get &  update private variable value.
+var counter = (function() {
+    var count = 0;
+   getValue = function() {
+          return count;
+      };
+  
+      setValue = function(value) {
+          count = value;
+      };
+   
+    return {
+      add: function(num) {
+        count += num;
+      },
+      sub: function(num) {
+       count -= num;
+      },
+      mul: function(num) {
+       count *= num;
+      },
+      divide : function(num) {
+       count /= num;
+      },
+     
+      value: function() {
+        return count;
+      }
+    };   
+  })();
+  console.log(counter.value());//logs 0
+  setValue(4);
+  getValue(); // logs 4
+  console.log(counter.value()); // logs 4
+  counter.add(1);
+  counter.add(5);
+  console.log(counter.value()); // logs 10
+  counter.sub(1);
+  console.log(counter.value()); // logs 9
+  counter.mul(5);
+  console.log(counter.value()); // logs 45
+  counter.divide(2);
+  console.log(counter.value()); // 22.5
