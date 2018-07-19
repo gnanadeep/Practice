9f08a14b4fce30c87f013c49e3af2ff565618c77
gnana <chandugnanadeep@gmail.com>
2018-07-19 15:27:35 -0400

    Rename day3.js to day4.js

---
+++ day4.js
@@ -0,0 +1,14 @@
+// Expressions;
+
+// 1)write code add, subtract, multiplication,division two numbers using variables.
+var a ,b
+var add = a+b;
+var sub = a-b;
+var mul = a*b;
+var div = a/b;
+
+// 2) pre and post increment and decrement of variables short hand notations.
+var i ;
+++i; //pre 
+ i++; //post
+// 3) Logical and / or / not operations using variables <, >, <=, >= , ==, ===  conditions.
