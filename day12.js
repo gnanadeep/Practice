54860da5266d1ef36548d97ffaea50f82c538356
chandu <chandugnanadeep@gmail.com>
2018-07-31 22:13:43 -0400

    'changes'

--- day12.js
+++ day12.js
@@ -15,22 +15,22 @@ var counter = Counter();
 // 2) Create a function with private properties such productId , name, price, 
 //    return an object contains methods to access the properties in the above function.
 
-  
+
 // 3) create an object having 2  properties, one contains array of numbers. the other 
 //  is a method is used to calculate the sum of the array elements in the above mentioned property.
 var obj = {
-    arr : [1,2,3,4],
-    sum : function(){
-       return this.arr.reduce((acc,cur) => acc + cur );
+    arr: [1, 2, 3, 4],
+    sum: function () {
+        return this.arr.reduce((acc, cur) => acc + cur);
     }
 }
 // 4) Create a new  student object which has properties such as name, rollno, marks in one property.
 var student = {
-    name : 'k',
+    name: 'k',
     rollno: 7,
-    marks : [1,2,3,4,5]
+    marks: [1, 2, 3, 4, 5]
 }
 // 5) Caclaute the sum of marks by using call and apply methods.
- 
 
+obj.sum.call(student.marks)
 // 6) Try to create a counter using IIFE.
