5946f50608e574ad1f4c868b359f08c1b2dfe678
chandu <chandugnanadeep@gmail.com>
2018-08-03 00:02:43 -0400

    ''

--- day14.js
+++ day14.js
@@ -3,11 +3,36 @@
 // 1) Write two functions to calculate the area and perimeter of a circle takes the radius 
 // as parameter.
 let area = r => Math.PI * r * r;
-console.log(area(1));
+console.log(area(2));
 
 // 2) Write a function to create a copy of an object by iterating over the keys of an it.
+
+const obj = {
+    name: "dev",
+    age: 21,
+    school: "king"
+}
+
+
+console.log(obj);
 // 3) Write a function to print the output as follows for the given no of rows with a given 
 // character.(given rows count is 6 and character is *){ two params (count, character)}
+
+var outStars = (count, character) => {
+
+    let str = '';
+    for (var i = 1; i <= count; i++) {
+        for (var j = 0; j < i; j++) {
+            str += character;
+
+        }
+        console.log(str);
+    }
+
+}
+
+console.log(outStars(4, '*'))
+
 // *
 // **
 // ***
