c1f938eb8ad04142848a97e1582aa0e6c0de9f6c
gnana <chandugnanadeep@gmail.com>
2018-08-05 00:53:07 -0400

    Update day15.js

--- day15.js
+++ day15.js
@@ -1,6 +1,7 @@
 // Day 15:
 // /*************** Arrow functions   ************************/
 // 1) In the given array, list out all even numbers (use arrow function in possible ways in writing this code)
-// 2) Write a programe to display numbers from 1 to 10 ,to dispaly each value it takes 5 seconds time between the any two values. {using arrow function}
+// 2) Write a programe to display numbers from 1 to 10 ,to dispaly each value it takes 5 seconds time between the any two values.
+// {using arrow function}
 // 3) Use arrow functions as call back function for the available array functions { write  atleast 4 arrow functions} .
 // 4) 
